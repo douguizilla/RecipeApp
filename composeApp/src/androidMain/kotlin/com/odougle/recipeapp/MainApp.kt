6018a6c2f2edd3c05d6c14e0c35c3ce1fe912bbf
Douglas Gomes <douglasgommez@gmail.com>
2025-07-15 23:57:44 -0300

    feat: Provide DatabaseFactory through Koin
    This commit configures Koin to provide `DatabaseFactory` instances for both Android and iOS platforms.
    
    On Android, `DatabaseFactory` is initialized with the `applicationContext` and provided as a singleton.
    On iOS, `DatabaseFactory` is provided as a singleton without any specific constructor arguments.

--- composeApp/src/androidMain/kotlin/com/odougle/recipeapp/MainApp.kt
+++ composeApp/src/androidMain/kotlin/com/odougle/recipeapp/MainApp.kt
@@ -1,13 +1,14 @@
 package com.odougle.recipeapp
 
 import android.app.Application
+import com.odougle.recipeapp.dbFactory.DatabaseFactory
 import com.odougle.recipeapp.di.initKoin
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 class MainApp : Application() {
     private val androidModules = module{
-
+        single { DatabaseFactory(applicationContext) }
     }
 
     override fun onCreate() {
--- composeApp/src/iosMain/kotlin/com/odougle/recipeapp/KoinIos.kt
+++ composeApp/src/iosMain/kotlin/com/odougle/recipeapp/KoinIos.kt
@@ -1,10 +1,11 @@
 package com.odougle.recipeapp
 
+import com.odougle.recipeapp.dbFactory.DatabaseFactory
 import com.odougle.recipeapp.di.initKoin
 import org.koin.dsl.module
 
 val iosModules = module {
-
+    single{ DatabaseFactory() }
 }
 
 fun initKoinIOS() = initKoin(additionalModule = listOf(iosModules))
