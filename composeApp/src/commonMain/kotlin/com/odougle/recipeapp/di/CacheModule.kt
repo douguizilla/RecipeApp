63d318e7635e239a0011752372a61de2caa51e42
Douglas Gomes <douglasgommez@gmail.com>
2025-07-16 23:04:45 -0300

    feat: Provide CoroutineScope and DAO through Koin
    This commit configures Koin to provide a `CoroutineScope` and a `RecipeDao` instance.
    
    - A `CoroutineScope` is provided as a singleton, using `Dispatchers.Default` as its `CoroutineContext`.
    - `DbHelper` is provided as a singleton.
    - `RecipeDao` is provided as a singleton, depending on `DbHelper`.

--- composeApp/src/commonMain/kotlin/com/odougle/recipeapp/di/CacheModule.kt
+++ composeApp/src/commonMain/kotlin/com/odougle/recipeapp/di/CacheModule.kt
@@ -1,7 +1,15 @@
 package com.odougle.recipeapp.di
 
+import com.odougle.recipeapp.features.commom.data.database.DbHelper
+import com.odougle.recipeapp.features.commom.data.database.dao.RecipeDao
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import org.koin.dsl.module
+import kotlin.coroutines.CoroutineContext
 
 fun cacheModule() = module {
-
+    single<CoroutineContext>{ Dispatchers.Default }
+    single { CoroutineScope(get()) }
+    single{ DbHelper(get()) }
+    single{ RecipeDao(get()) }
 }
