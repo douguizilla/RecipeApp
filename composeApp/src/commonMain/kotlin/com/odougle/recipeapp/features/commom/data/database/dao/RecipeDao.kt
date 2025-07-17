db665468dc3b68e4c861c34e6ddf961041ca0148
Douglas Gomes <douglasgommez@gmail.com>
2025-07-16 23:05:00 -0300

    feat: Create RecipeDao
    This commit introduces a `RecipeDao` class.
    
    The `RecipeDao` takes a `DbHelper` as a constructor parameter and currently includes an empty `clearDatabase()` function.

---
+++ composeApp/src/commonMain/kotlin/com/odougle/recipeapp/features/commom/data/database/dao/RecipeDao.kt
@@ -0,0 +1,9 @@
+package com.odougle.recipeapp.features.commom.data.database.dao
+
+import com.odougle.recipeapp.features.commom.data.database.DbHelper
+
+class RecipeDao(
+    private val dbHelper: DbHelper
+) {
+    fun clearDatabase(){}
+}
