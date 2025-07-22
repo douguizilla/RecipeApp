cf8cd559a4840b8929c1f3fe154b2bd75f3ad149
Douglas Gomes <douglasgommez@gmail.com>
2025-07-21 22:24:40 -0300

    feat: Implement RecipeDao for database operations
    This commit introduces the `RecipeDao` class, which provides methods for interacting with the recipe database using SQLDelight.
    
    The `RecipeDao` includes the following functionalities:
    - `insertRecipe(recipeItem: RecipeItem)`: Inserts a single recipe into the database.
    - `updateRecipe(recipeItem: RecipeItem)`: Updates an existing recipe in the database.
    - `insertRecipesBulk(recipes: List<RecipeItem>)`: Inserts a list of recipes in bulk.
    - `upsertRecipesBulk(recipes: List<RecipeItem>)`: Inserts or updates a list of recipes in bulk.
    - `getAllRecipes(): List<RecipeItem>`: Retrieves all recipes from the database.
    - `getRecipeById(id: Long): RecipeItem?`: Retrieves a specific recipe by its ID.
    - `deleteRecipeById(id: Long)`: Deletes a recipe by its ID.
    - `searchRecipesByText(text: String): List<RecipeItem>`: Searches for recipes based on a text query.
    
    All database operations are performed asynchronously using `dbHelper.withDatabase` and SQLDelight's coroutine extensions (`awaitAsList`, `awaitAsOneOrNull`). The `recipeEntityMapper` is used to map database entities to `RecipeItem` domain objects.

--- composeApp/src/commonMain/kotlin/com/odougle/recipeapp/features/commom/data/database/dao/RecipeDao.kt
+++ composeApp/src/commonMain/kotlin/com/odougle/recipeapp/features/commom/data/database/dao/RecipeDao.kt
@@ -1,9 +1,130 @@
 package com.odougle.recipeapp.features.commom.data.database.dao
 
+import app.cash.sqldelight.async.coroutines.awaitAsList
+import app.cash.sqldelight.async.coroutines.awaitAsOneOrNull
 import com.odougle.recipeapp.features.commom.data.database.DbHelper
+import com.odougle.recipeapp.features.commom.data.database.recipeEntityMapper
+import com.odougle.recipeapp.features.commom.domain.entities.RecipeItem
 
 class RecipeDao(
     private val dbHelper: DbHelper
 ) {
-    fun clearDatabase(){}
+    suspend fun insertRecipe(recipeItem: RecipeItem) {
+        dbHelper.withDatabase { database ->
+            database.recipeEntityQueries.insertRecipe(
+                recipeItem.id,
+                recipeItem.title,
+                recipeItem.description,
+                recipeItem.category,
+                recipeItem.area,
+                recipeItem.imageUrls,
+                recipeItem.youtubeUrl,
+                recipeItem.ingredients,
+                recipeItem.instructions,
+                if (recipeItem.isFavorite) 1 else 0,
+                recipeItem.rating,
+                recipeItem.duration,
+                recipeItem.difficulty
+            )
+        }
+    }
+
+    suspend fun updateRecipe(recipeItem: RecipeItem) {
+        dbHelper.withDatabase { database ->
+            database.recipeEntityQueries.updateRecipe(
+                recipeItem.title,
+                recipeItem.description,
+                recipeItem.category,
+                recipeItem.area,
+                recipeItem.imageUrls,
+                recipeItem.youtubeUrl,
+                recipeItem.ingredients,
+                recipeItem.instructions,
+                if (recipeItem.isFavorite) 1 else 0,
+                recipeItem.rating,
+                recipeItem.duration,
+                recipeItem.difficulty,
+                recipeItem.id
+            )
+        }
+    }
+
+    suspend fun insertRecipesBulk(recipes: List<RecipeItem>) {
+        dbHelper.withDatabase { database ->
+            recipes.forEach { recipeItem ->
+                database.recipeEntityQueries.insertRecipe(
+                    recipeItem.id,
+                    recipeItem.title,
+                    recipeItem.description,
+                    recipeItem.category,
+                    recipeItem.area,
+                    recipeItem.imageUrls,
+                    recipeItem.youtubeUrl,
+                    recipeItem.ingredients,
+                    recipeItem.instructions,
+                    if (recipeItem.isFavorite) 1 else 0,
+                    recipeItem.rating,
+                    recipeItem.duration,
+                    recipeItem.difficulty
+                )
+            }
+        }
+    }
+
+    suspend fun upsertRecipesBulk(recipes: List<RecipeItem>) {
+        dbHelper.withDatabase { database ->
+            recipes.forEach { recipeItem ->
+                database.recipeEntityQueries.upsertRecipe(
+                    recipeItem.title,
+                    recipeItem.description,
+                    recipeItem.category,
+                    recipeItem.area,
+                    recipeItem.imageUrls,
+                    recipeItem.youtubeUrl,
+                    recipeItem.ingredients,
+                    recipeItem.instructions,
+                    if (recipeItem.isFavorite) 1 else 0,
+                    recipeItem.rating,
+                    recipeItem.duration,
+                    recipeItem.difficulty,
+                    recipeItem.id,
+                )
+            }
+        }
+    }
+
+    suspend fun getAllRecipes(): List<RecipeItem> {
+
+        return dbHelper.withDatabase { database ->
+            database.recipeEntityQueries.selectAllRecipes().awaitAsList().map {
+                recipeEntityMapper(it)
+            }
+        }
+
+    }
+
+    suspend fun getRecipeById(id: Long): RecipeItem? {
+
+        return dbHelper.withDatabase { database ->
+            database.recipeEntityQueries.selectRecipeById(id).awaitAsOneOrNull()?.let {
+                recipeEntityMapper(it)
+            }
+        }
+
+    }
+
+    suspend fun deleteRecipeById(id: Long) {
+        dbHelper.withDatabase { database ->
+            database.recipeEntityQueries.deleteRecipeById(id)
+        }
+    }
+
+
+    suspend fun searchRecipesByText(text: String): List<RecipeItem> {
+        return dbHelper.withDatabase { database ->
+            database.recipeEntityQueries.searchRecipeByText(text).awaitAsList().map {
+                recipeEntityMapper(it)
+            }
+        }
+    }
 }
