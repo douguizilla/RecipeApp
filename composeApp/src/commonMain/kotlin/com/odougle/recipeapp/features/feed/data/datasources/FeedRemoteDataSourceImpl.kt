b998ea14bc15440c1b46ae51b85fb1a2263ca28a
Douglas Gomes <douglasgommez@gmail.com>
2025-07-21 22:56:53 -0300

    feat: Implement recipe fetching in FeedRemoteDataSourceImpl
    This commit implements the `getRecipesList` function in `FeedRemoteDataSourceImpl`.
    
    Key changes:
    - Injects `HttpClient` into `FeedRemoteDataSourceImpl`.
    - Fetches recipes from the `${BASE_URL}search.php?f=b` endpoint.
    - Deserializes the JSON response into a `RecipeListDTO`.
    - Maps the DTO to a list of `RecipeItem` entities, filtering out null results from the `toRecipe()` conversion.

--- composeApp/src/commonMain/kotlin/com/odougle/recipeapp/features/feed/data/datasources/FeedRemoteDataSourceImpl.kt
+++ composeApp/src/commonMain/kotlin/com/odougle/recipeapp/features/feed/data/datasources/FeedRemoteDataSourceImpl.kt
@@ -1,9 +1,21 @@
 package com.odougle.recipeapp.features.feed.data.datasources
 
+import com.odougle.recipeapp.features.commom.data.api.BASE_URL
+import com.odougle.recipeapp.features.commom.data.model.RecipeListDTO
+import com.odougle.recipeapp.features.commom.data.model.toRecipe
 import com.odougle.recipeapp.features.commom.domain.entities.RecipeItem
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
 
-class FeedRemoteDataSourceImpl : FeedRemoteDataSource {
+class FeedRemoteDataSourceImpl(
+    private val httpClient: HttpClient
+) : FeedRemoteDataSource {
     override suspend fun getRecipesList() : List<RecipeItem>{
-        TODO()
+        val httpResponse = httpClient.get("${BASE_URL}search.php?f=b")
+        val recipeListApiResponse = httpResponse.body<RecipeListDTO>()
+        return recipeListApiResponse.meals.mapNotNull {
+            it.toRecipe()
+        }
     }
 }
