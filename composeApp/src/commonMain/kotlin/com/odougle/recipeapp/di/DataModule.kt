7d48832c9b3e1d24952051dd44ba3d0f11b208a1
Douglas Gomes <douglasgommez@gmail.com>
2025-07-21 22:56:32 -0300

    feat: Provide Feed DataSources through Koin
    This commit configures Koin to provide implementations for `FeedLocalDataSource` and `FeedRemoteDataSource`.
    
    - `FeedLocalDataSourceImpl` is provided as a singleton, taking a `DatabaseFactory` as a dependency.
    - `FeedRemoteDataSourceImpl` is provided as a singleton, taking an `HttpClient` as a dependency.

--- composeApp/src/commonMain/kotlin/com/odougle/recipeapp/di/DataModule.kt
+++ composeApp/src/commonMain/kotlin/com/odougle/recipeapp/di/DataModule.kt
@@ -1,7 +1,12 @@
 package com.odougle.recipeapp.di
 
+import com.odougle.recipeapp.features.feed.data.datasources.FeedLocalDataSource
+import com.odougle.recipeapp.features.feed.data.datasources.FeedLocalDataSourceImpl
+import com.odougle.recipeapp.features.feed.data.datasources.FeedRemoteDataSource
+import com.odougle.recipeapp.features.feed.data.datasources.FeedRemoteDataSourceImpl
 import org.koin.dsl.module
 
 fun dataModule() = module {
-    
+    single<FeedLocalDataSource> { FeedLocalDataSourceImpl(get()) }
+    single<FeedRemoteDataSource> { FeedRemoteDataSourceImpl(get()) }
 }
