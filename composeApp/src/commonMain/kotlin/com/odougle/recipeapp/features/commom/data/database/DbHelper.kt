95b79945481d4e037bd4dae5109bf31f06040d7e
Douglas Gomes <douglasgommez@gmail.com>
2025-07-21 22:24:31 -0300

    feat: Initialize Recipe adapter in DbHelper
    This commit updates the `DbHelper` class to initialize the `RecipeAdapter` when creating the database instance.
    
    The `createDb` function now takes the `driverFactory` as a parameter and initializes `RecipeAppCmpAppDb` with the driver and a `Recipe.Adapter`. The adapter is configured to use `listOfStringsAdapter` for both `ingredientsAdapter` and `instructionsAdapter`.
    
    The `withDatabase` function's return type has been changed to `Result : Any?` to accommodate potentially nullable results from the database operations.

--- composeApp/src/commonMain/kotlin/com/odougle/recipeapp/features/commom/data/database/DbHelper.kt
+++ composeApp/src/commonMain/kotlin/com/odougle/recipeapp/features/commom/data/database/DbHelper.kt
@@ -1,5 +1,6 @@
 package com.odougle.recipeapp.features.commom.data.database
 
+import com.odougle.recipeapp.Recipe
 import com.odougle.recipeapp.RecipeAppCmpAppDb
 import com.odougle.recipeapp.dbFactory.DatabaseFactory
 import kotlinx.coroutines.sync.Mutex
@@ -8,13 +9,26 @@ import kotlinx.coroutines.sync.withLock
 class DbHelper(
     private val driverFactory: DatabaseFactory
 ) {
+
     private var db: RecipeAppCmpAppDb? = null
     private val mutex = Mutex()
 
-    suspend fun <Result: Any> withDatabase(block: suspend (RecipeAppCmpAppDb) -> Result) = mutex.withLock {
-        if (db == null) db = createDb(driverFactory)
-        return@withLock block(db!!)
-    }
+    suspend fun <Result : Any?> withDatabase(block: suspend (RecipeAppCmpAppDb) -> Result) =
+        mutex.withLock {
+            if (db == null) {
+                db = createDb(driverFactory)
+            }
 
-    private suspend fun createDb(driverFactory: DatabaseFactory) = RecipeAppCmpAppDb(driver = driverFactory.createDriver())
+            return@withLock block(db!!)
+        }
+
+    private suspend fun createDb(driverFactory: DatabaseFactory): RecipeAppCmpAppDb {
+        return RecipeAppCmpAppDb(
+            driver = driverFactory.createDriver(),
+            RecipeAdapter = Recipe.Adapter(
+                ingredientsAdapter = listOfStringsAdapter,
+                instructionsAdapter = listOfStringsAdapter
+            )
+        )
+    }
 }
